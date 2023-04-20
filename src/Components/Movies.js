0adf3c486495eafad3797f4b7c80e5d2ff9d0535
Kenya Womack <ladykamillion27@gmail.com>
2023-04-20 02:02:07 -0400

    add movie poster

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -1,15 +1,11 @@
 import React, { useState } from 'react';
 // import { Carousel } from 'react-bootstrap';
 import Movie from './Movie';
-
-
 class Movies extends React.Component {
-
   render() {
     return (
       <Movie  movie={this.props.movies} />
     );
   };
 }
-
 export default Movies;
