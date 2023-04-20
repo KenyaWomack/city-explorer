da3358399aca0f642f84d08eefe861054621c0c8
Kenya Womack <ladykamillion27@gmail.com>
2023-04-19 20:49:11 -0400

    front end complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
-import React from 'react';
-import Header from './components/Header'
-import Main from './components/Main'
-import Footer from './components/Footer';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import './App.css';
+import Main from './components/main';
 
-class App extends React.Component {
-  render(){
-    return (
-      <>
-        <Header />
-        <Main />
-        <Footer />
-      </>
-    )
-  }
+function App() {
+  const [city, setCity] = useState('');
+  const [lat, setLat] = useState('');
+  const [lon, setLon] = useState('');
+  const [mapIsDisplaying, setMapIsDisplaying] = useState(false);
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleCitySubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let cityUrl = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_IQ_API_KEY}&q=${city}&format=json`;
+      let cityInfo = await axios.get(cityUrl);
+
+      setLat(cityInfo.data[0].lat);
+      setLon(cityInfo.data[0].lon);
+      setMapIsDisplaying(true);
+      setError(false);
+    } catch (error) {
+      setError(true);
+      setErrorMessage(error.message);
+    }
+  };
+
+  const cityChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  return (
+    <>
+      <Main />
+    </>
+  )
 }
+// </>
+// );
+// }
 
 export default App;
